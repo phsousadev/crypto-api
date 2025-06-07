f8e5193627ed7d90c62562d590318492c4dc2bd7
Pedro Sousa <pedro.sousa@pensaredu.com.br>
2025-06-07 16:51:54 -0300

    feat: implementing logs

--- src/http/use-cases/decrypt-text-use-case.ts
+++ src/http/use-cases/decrypt-text-use-case.ts
@@ -1,4 +1,5 @@
 import { DescryptionFailedError } from 'http/errors/decryption-failed'
+import { logger } from 'lib/logger'
 import { EncryptedTextRepository } from 'repositories/encrypt-text'
 import { decryptText } from 'utils/crypt.utils'
 
@@ -18,7 +19,23 @@ export class DecryptUseCase {
   async execute(input: DecryptInput): Promise<DecryptResult> {
     const decrypted = decryptText(input.encrypted, input.key, input.iv)
 
-    if (!decrypted) throw new DescryptionFailedError()
+    if (!decrypted) {
+      logger.warn({
+        msg: 'Failed to decrypt',
+        data: {
+          keyPreview: input.key.slice(0, 6),
+          ivPreview: input.iv.slice(0, 6),
+        },
+      })
+      throw new DescryptionFailedError()
+    }
+
+    logger.info({
+      msg: 'Text decrypted successfully',
+      data: {
+        length: decrypted.originalText.length,
+      },
+    })
 
     return {
       decrypted: decrypted.originalText,
--- src/http/use-cases/encrypt-text-use-case.ts
+++ src/http/use-cases/encrypt-text-use-case.ts
@@ -1,4 +1,5 @@
 import { EncryptionFailedError } from 'http/errors/encryption-failed'
+import { logger } from 'lib/logger'
 import { EncryptedTextRepository } from 'repositories/encrypt-text'
 import { encryptText } from 'utils/crypt.utils'
 
@@ -18,14 +19,43 @@ export class CreateEncryptTextUseCase {
   async execute({
     text,
   }: CreateEncryptTextUseCaseRequest): Promise<CreateEncryptTextUseCaseResponse> {
-    if (!text) throw new EncryptionFailedError()
+    if (!text) {
+      logger.warn({
+        msg: 'Received empty text for encryption',
+      })
+      throw new EncryptionFailedError()
+    }
 
-    const { encryptedText, key, iv } = encryptText(text)
+    logger.info({
+      msg: 'Iniciando criptografia',
+      meta: {
+        length: text.length,
+        preview: text.slice(0, 10),
+      },
+    })
 
-    return {
-      encryptedText,
-      key,
-      iv,
+    try {
+      const { encryptedText, key, iv } = encryptText(text)
+
+      logger.info({
+        msg: 'Encryption successful',
+        meta: {
+          encryptedLength: encryptedText.length,
+        },
+      })
+
+      return {
+        encryptedText,
+        key,
+        iv,
+      }
+    } catch (error) {
+      logger.error({
+        msg: 'Erro durante criptografia',
+        err: error,
+      })
+
+      throw new EncryptionFailedError()
     }
   }
 }
